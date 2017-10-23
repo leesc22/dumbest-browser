83e7d6625937db161cae48a132b718c06147d8a4
leesc22 <leesiewching22@gmail.com>
2017-10-23 14:56:34 +0800

    Complete world's dumbest browser exercise

--- browser.rb
+++ browser.rb
@@ -1,9 +1,13 @@
 require 'net/http'
 require 'nokogiri'
+require 'open-uri'
 
-require_relative 'util'
+require_relative 'page'
 
 class Browser
+  def initialize
+  end
+
   def run!
     # Run the browser
     # Display a prompt for a user
@@ -14,6 +18,27 @@ class Browser
     # Questions:
     #  1. How can a user quit the browser gracefully?
     #  2. How can a user ask for help, i.e., how do they know what commands are available to them?
+
+    print "url> "
+    input = gets.chomp
+    puts "Fetching..."
+
+    page = Page.new(input)
+
+    # Makes the HTTP request
+    page.fetch!
+
+    # Returns the URL of the underlying page
+    page.url
+
+    # Returns the page's title
+    puts "Title: #{page.title}"
+
+    puts "Content length: #{page.response.content.length} characters"
+
+    # Returns an Array of URLs (as Strings)
+    puts "Links: "
+    puts page.links
   end
 end
 
--- page.rb
+++ page.rb
@@ -1,13 +1,22 @@
 class Page
+  attr_reader :url, :response
+
   def initialize(url)
+    @url = url
+    @response
   end
   
+  # Makes the HTTP request
   def fetch!
+    @response = Nokogiri::HTML(open(@url))
   end
   
+  # Returns the page's title
   def title
+    @response.css("title").text
   end
   
+  # Returns an Array of URLs (as Stringss)
   def links
     # Research alert!
     # How do you use Nokogiri to extract all the link URLs on a page?
@@ -15,5 +24,11 @@ class Page
     # These should only be URLs that look like
     #   <a href="http://somesite.com/page.html">Click here!</a>
     # This would pull out "http://somesite.com/page.html"
+    @response.css("a").map { |link| link["href"] }
   end
 end
+
+# References:
+# http://www.rubyinside.com/nethttp-cheat-sheet-2940.html
+# http://www.nokogiri.org/tutorials/parsing_an_html_xml_document.html
+# http://ruby.bastardsbook.com/chapters/html-parsing/
